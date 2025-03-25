cb142888cb111ce4edf1d1528cba07c286385d06
Albert Nigmatullin <albertnig16@gmail.com>
2025-03-25 22:08:57 +0300

    Commit Module Test

--- src/test/java/JUnit/Vector2DTest.java
+++ src/test/java/JUnit/Vector2DTest.java
@@ -4,24 +4,25 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Vector2DTest {
+    private final double EPS = 1e-9;
     @Test
     public void newVectorShouldHaveZeroLength(){
         Vector2D v1 = new Vector2D(); //action
         //assertion
-        Assertions.assertEquals(0, v1.length(), 1e-9);
+        Assertions.assertEquals(0, v1.length(), EPS);
     }
 
     @Test
     public void newVectorShouldHaveZeroX(){
         Vector2D v1 = new Vector2D();
         //assertion
-        Assertions.assertEquals(0, v1.getX(), 1e-9);
+        Assertions.assertEquals(0, v1.getX(), EPS);
     }
 
     @Test
     public void newVectorShouldHaveZeroY(){
         Vector2D v1 = new Vector2D();
         //assertion
-        Assertions.assertEquals(0, v1.getY(), 1e-9);
+        Assertions.assertEquals(0, v1.getY(), EPS);
     }
 }
